b11f573b6d090dded00c915fb30a7efb61254bc7
KEV <kev789@ya.ru>
2026-06-11 11:03:39 +0300

    Добавлен параметр /ClearCache для очистки кэша клиент-серверных вызовов

--- app/src/main/java/l1c/RunYBase.java
+++ app/src/main/java/l1c/RunYBase.java
@@ -106,6 +106,7 @@ public class RunYBase extends Application {
     private ToggleGroup modeGroup;
     private CheckBox priorityPlatformCheckbox;
     private CheckBox debugModeCheckbox;
+    private CheckBox clearCacheCheckbox;
     private ComboBox<String> debugProtocolCombo;
     private TextArea debugArea;
     private ObservableList<String> historyList;
@@ -428,9 +429,18 @@ public class RunYBase extends Application {
         helpButton.setOnAction(e -> showAlert(Alert.AlertType.INFORMATION, "Справка: параметр /AppArch",
                 RunYBaseHelpTexts.APP_ARCH_INFO));
 
+        clearCacheCheckbox = new CheckBox("Очистка кэша (/ClearCache)");
+        clearCacheCheckbox.setTooltip(createTooltip(RunYBaseHelpTexts.CLEAR_CACHE_TOOLTIP));
+
+        Button clearCacheHelpButton = createHelpButton();
+        clearCacheHelpButton.setTooltip(createTooltip(RunYBaseHelpTexts.CLEAR_CACHE_INFO));
+        clearCacheHelpButton.setOnAction(e -> showAlert(Alert.AlertType.INFORMATION, "Справка: параметр /ClearCache",
+                RunYBaseHelpTexts.CLEAR_CACHE_INFO));
+
         HBox debugOption = createHelpOption(debugModeCheckbox, debugProtocolCombo, debugHelpButton);
         HBox platformOption = createHelpOption(priorityPlatformCheckbox, helpButton);
-        HBox optionsRow = new HBox(15, debugOption, platformOption);
+        HBox clearCacheOption = createHelpOption(clearCacheCheckbox, clearCacheHelpButton);
+        HBox optionsRow = new HBox(15, debugOption, platformOption, clearCacheOption);
         optionsRow.setAlignment(Pos.CENTER_LEFT);
 
         panel.getChildren().addAll(modeRow, optionsRow);
@@ -856,6 +866,10 @@ public class RunYBase extends Application {
                 debugParam += " " + protocol;
             }
             cmd.append(debugParam);
+        }
+
+        if (clearCacheCheckbox.isSelected()) {
+            cmd.append(" /ClearCache");
         }
 
         return cmd.toString();
