9fd35d605e6c497d714ba320dd3d8fa1b6e7feb7
KEV <kev789@ya.ru>
2026-06-27 15:33:34 +0300

    Версия повышена

--- app/src/main/java/l1c/AppConstants.java
+++ app/src/main/java/l1c/AppConstants.java
@@ -3,7 +3,7 @@ package l1c;
 public class AppConstants {
     
     // Версия приложения
-    public static final String VERSION = "2026.06.17.006";
+    public static final String VERSION = "2026.06.27.007";
     
     // @formatter:off
     public static final String APP_DATA_DIR        = ".1c_launcher";
